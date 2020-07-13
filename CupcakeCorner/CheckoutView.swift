3b15d927c3d9c439eb2b5eab1da093e7439694b0
Barry Martin <bymartin@users.noreply.github.com>
2020-07-13 11:30:34 -0500

    Project 10, Part 2, Day 50 of 100 days of SwiftUi

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,41 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Barry Martin on 7/13/20.
+//  Copyright © 2020 Barry Martin. All rights reserved.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @ObservedObject var order: Order
+    
+    var body: some View {
+        GeometryReader { geo in
+            ScrollView {
+                VStack {
+                    Image("cupcakes")
+                    .resizable()
+                    .scaledToFit()
+                        .frame(width: geo.size.width)
+                    
+                    Text("Your total is $\(self.order.cost, specifier: "%.2f")")
+                        .font(.title)
+                    
+                    Button("Place order") {
+                        // place order
+                    }
+                .padding()
+                }
+            }
+        }
+        .navigationBarTitle("Check out", displayMode: .inline)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView(order: Order())
+    }
+}
